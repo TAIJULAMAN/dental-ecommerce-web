c6f41c09189c969dd7f119f20db03f726e17b13a
TAIJULAMAN <taijulaman8@gmail.com>
2025-07-23 01:11:37 +0600

    pharmaceucle complete

--- src/pages/pharmaceuticals/Pharmaceuticals.jsx
+++ src/pages/pharmaceuticals/Pharmaceuticals.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 export default function Pharmaceuticals() {
   return (
-    <div className="flex justify-between items-center h-[500px]">
+    <div className="flex justify-between items-center container mx-auto py-10">
       <div className="w-1/2">
-        <h1>Comming Soon</h1>
-        <p>
+        {/* <h1 className="text-7xl font-bold text-white">Comming Soon</h1> */}
+        <img src="/mask.png" alt="doc pic" className="w-full h-full" />
+        <p className="text-gray-600 text-xl mt-5">
           We are working hard on this page We will be back soon please visit
           again
         </p>
       </div>
-      <div className="w-1/2">
-        <img src="./doc2.png" alt="doc pic" className="w-full h-full object-contain" />
+      <div className="w-1/2 flex justify-end items-end">
+        <img src="./doc2.png" alt="doc pic" className="w-[400px] h-[600px] object-contain" />
       </div>
     </div>
   );
